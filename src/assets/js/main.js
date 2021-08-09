ac02d652c60fc6d28da077c1eac1afde3f11cbef
Manouchehr <manouchehr.ghale@gmail.com>
2021-08-09 17:51:50 +0430

    create product Mobile html and style

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,25 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 1, 2023 15:37:25").getTime();
+const countDownDate = new Date("August 1, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+const x = setInterval(function () {
     // Get today's date and time
-    var now = new Date().getTime();
+    const now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    const distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
     // document.getElementById("demo").innerHTML ='<span id="day">'+ days + '</span>' +  '<span id="hours">'+ hours + '</span>'
     // + '<span id="minutes">'+ minutes + '</span>' + '<span id="seconds">'+ seconds + '</span>';
-    var z = document.getElementsByClassName("demos");
-    for (var i = 0; i < z.length; i++) {
+    const z = document.getElementsByClassName("demos");
+    for (let i = 0; i < z.length; i++) {
         z[i].innerHTML =
             '<span id="day">' +
             days +
@@ -39,27 +39,27 @@ var x = setInterval(function () {
     if (distance < 0) {
         clearInterval(x);
         // document.getElementById("demo").innerHTML = "";
-        var y = document.getElementsByClassName("demos");
-        for (var i = 0; i < z.length; i++) {
+        const y = document.getElementsByClassName("demos");
+        for (let i = 0; i < z.length; i++) {
             y[i].innerHTML = "مهلت تخفیف به پایان رسیده است";
         }
 
         // document.getElementById("after-expire").setAttribute("id","offer-expire-text");
-        var g = document.getElementsByClassName("after-expire");
-        for (var i = 0; i < z.length; i++) {
+        const g = document.getElementsByClassName("after-expire");
+        for (let i = 0; i < z.length; i++) {
             g[i].classList.add("offer-expire-text");
         }
 
         // document.getElementById("offer-expire-text-inner").innerHTML = "پیشنهاد ویژه این محصول به پایان رسیده!";
-        var d = document.getElementsByClassName("offer-expire-text-inner");
-        for (var i = 0; i < z.length; i++) {
+        const d = document.getElementsByClassName("offer-expire-text-inner");
+        for (let i = 0; i < z.length; i++) {
             d[i].innerHTML = "پیشنهاد ویژه این محصول به پایان رسیده!";
             d[i].classList.add("badge")
         }
 
         // document.getElementById("offer-blur").style.filter = "blur(2px)";
-        var t = document.getElementsByClassName("offer-blur");
-        for (var i = 0; i < z.length; i++) {
+        const t = document.getElementsByClassName("offer-blur");
+        for (let i = 0; i < z.length; i++) {
             t[i].style.filter = "blur(2px)";
         }
     }
@@ -77,7 +77,7 @@ $(document).ready(function () {
         center: true,
         autoplay: true,
         slideBy:3,
-        autoplayTimeout: 5000,
+        autoplayTimeout: 3000,
         autoplayHoverPause: true,
         responsiveClass:true,
         responsive:{
